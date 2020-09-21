3fea83180c1aa5fe44430e41761c37c6a20068db
Belyaeva Ekaterina <katrinamalinashtorm@gmail.com>
2020-09-21 18:18:44 +0600

    Добавлены необходимые библиотеки для работы приложения

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import {createStore, applyMiddleware, compose, combineReducers} from "redux";
+import thunkMiddleware from 'redux-thunk';
+import cartReducer from "./store/reducers/cartReducer";
+import menuReducer from "./store/reducers/menuReducer";
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    menu: menuReducer
+});
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunkMiddleware))
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
